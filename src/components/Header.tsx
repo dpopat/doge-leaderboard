787381e75ff73e72546474f58ebad270163ded4e
Dhvanil Popat <dpopat99@gmail.com>
2024-11-19 22:02:59 -0600

    Update header with coin and bolder border

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
 import ShineBorder from "@/components/ui/shine-border";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full bg-white border-b border-gray-500">
+    <header className="w-full bg-white border-b-2 border-gray-500">
       <div className="p-4">
-        <h1 className="font-bold text-2xl">DOGELeaderboard</h1>
+        <h1 className="font-bold text-2xl flex items-center gap-2">
+          <Image
+            src="/doge-black.png"
+            alt="Doge Icon"
+            width={32}
+            height={32}
+            className="object-contain"
+            title="DOGE Leaderboard Logo"
+          />
+          DOGELeaderboard
+        </h1>
       </div>
       <ShineBorder borderRadius={0} borderWidth={0.5} color={["#d19e00", "#a67102"]}>
         <></>
