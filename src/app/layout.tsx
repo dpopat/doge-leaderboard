51fd1b45db77d504c53a8a89d50987698ab35995
Dhvanil Popat <dpopat99@gmail.com>
2024-11-24 13:01:36 -0600

    Add posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { CSPostHogProvider } from '@/app/providers/posthog-provider'
 import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,11 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <CSPostHogProvider>
       <body
         className={`${inter.className}`}
       >
         {children}
-      </body>
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }
