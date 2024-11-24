325c0217a893d1d448ce48b7dfe96bc78dc08ba9
Dhvanil Popat <dpopat99@gmail.com>
2024-11-24 13:12:03 -0600

    Log when NEXT_PUBLIC_POSTHOG_KEY is not set'

--- src/app/providers/posthog-provider.tsx
+++ src/app/providers/posthog-provider.tsx
@@ -5,9 +5,9 @@ import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
   if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-    throw new Error('NEXT_PUBLIC_POSTHOG_KEY is not set')
+    console.log('NEXT_PUBLIC_POSTHOG_KEY is not set')
   }
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
   })
